bc1472141ee54f9824ffe78ec2ff8530708fa153
jagoulartpuc <jose.goulart@acad.pucrs.br>
2024-04-16 11:10:51 -0300

    displaying characters with images

--- marvel_story_characters.rb
+++ marvel_story_characters.rb
@@ -12,20 +12,21 @@ def generate_auth_params
   { ts: ts, apikey: PUBLIC_KEY, hash: hash }
 end
 
-def fetch_stories_by_character_id(character_id)
+def fetch_character_info(character_id)
     auth_params = generate_auth_params
     response = HTTParty.get("#{MARVEL_API_BASE_URL}/characters/#{character_id}", query: auth_params)
     if response.code == 200
       data = response.parsed_response['data']
-      results = data['results']
-      character_name = results[0]['name']
-      stories = results[0]['stories']
-      if stories.any?
+      results = data['results']   
+      if results.any?
+        stories = results.first['stories']
         items = stories['items']
         stories_urls = items.map { |items| items['resourceURI'] }
-        return character_name, stories_urls
+        thumbnail = results.first['thumbnail']
+        image_url = "#{thumbnail['path']}.#{thumbnail['extension']}"
+        return stories_urls, image_url
       else
-        puts 'No stories found for this character.'
+        puts 'No results found for this character.'
       end
     else
       puts "Error: API request failed with code #{response.code}"
@@ -35,19 +36,19 @@ def fetch_stories_by_character_id(character_id)
   
 def fetch_random_story_with_character(character_id)
   auth_params = generate_auth_params
-  character_name, stories_url = fetch_stories_by_character_id(character_id)
+  stories_url, image_url = fetch_character_info(character_id)
   response = HTTParty.get(stories_url.sample, query: auth_params)
   if response.code == 200
     data = response.parsed_response['data']
     results = data['results']
     if results.any?
-      story = results.first
-      characters = story['characters']['items']
-      character_names = characters.map { |char| char['name'] }
-      if character_names.include?(character_name)
-        return story
-      end
+      story = results.first   
+      return story
+    else
+      puts 'No results found.'
     end
+  else
+    puts "Error: API request failed with code #{response.code}"
   end
 
   nil
@@ -59,7 +60,11 @@ def generate_marvel_story_html(character_name)
 
   description = story['description']
   characters = story['characters']['items']
-
+  for character in characters
+    character_id = character['resourceURI'].split('/').last
+    _, image_url = fetch_character_info(character_id)
+    character['resourceURI'] = image_url
+  end
   html_template = ERB.new(File.read('template.html.erb'))
   File.write('marvel_story.html', html_template.result(binding))
 end
